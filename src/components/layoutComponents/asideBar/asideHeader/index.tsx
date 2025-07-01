7c48b7d97798895cd8c389f8b5862bac3a7681f7
Guilherme Xavier <xguilherme1@gmail.com>
2025-07-01 16:09:54 -0300

    husky config

--- src/components/layoutComponents/asideBar/asideHeader/index.tsx
+++ src/components/layoutComponents/asideBar/asideHeader/index.tsx
@@ -9,41 +9,33 @@ import { useState } from 'react'
 
 interface IAsideHeaderParams {
   isCollapsed: boolean
-  setIsCollapsed: (e: boolean) => void,
+  setIsCollapsed: (e: boolean) => void
 }
 
-const AsideHeader = ({isCollapsed,setIsCollapsed}:IAsideHeaderParams) => {
+const AsideHeader = ({ isCollapsed, setIsCollapsed }: IAsideHeaderParams) => {
   const router = useRouter()
   const [openTabs, setOpenTabs] = useState<string[]>([])
   const pathname = usePathname()
 
-
   return (
-      <div>
-        <button
-          onClick={() => setIsCollapsed(!isCollapsed)}
-          className="absolute right-0 top-4 bg-primary text-white rounded-l-xl flex justify-center items-center"
-        >
-          {isCollapsed ? (
-            <ChevronRightIcon  />
-          ) : (
-            <ChevronLeftIcon  />
-          )}
-        </button>
+    <div>
+      <button
+        onClick={() => setIsCollapsed(!isCollapsed)}
+        className="absolute right-0 top-4 bg-primary text-white rounded-l-xl flex justify-center items-center"
+      >
+        {isCollapsed ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+      </button>
 
-        <div
-          className={`flex justify-center mb-6 ${
-            isCollapsed ? 'mt-10' : 'mt-6'
-          }`}
-        >
-          {isCollapsed ? (
-            <Image src={logoImg} alt="Logo" width={100} height={100} />
-          ) : (
-            <Logo minimal />
-          )}
-        </div>
+      <div
+        className={`flex justify-center mb-6 ${isCollapsed ? 'mt-10' : 'mt-6'}`}
+      >
+        {isCollapsed ? (
+          <Image src={logoImg} alt="Logo" width={100} height={100} />
+        ) : (
+          <Logo minimal />
+        )}
       </div>
-
+    </div>
   )
 }
 
