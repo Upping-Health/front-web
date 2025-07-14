4e5091052ebcd06c6e07f158d1fa7ab1fe3b041c
Guilherme Xavier <xguilherme1@gmail.com>
2025-07-14 09:44:03 -0300

    adjusts

--- src/app/(dashboard)/patients/[id]/anthropometry/page.tsx
+++ src/app/(dashboard)/patients/[id]/anthropometry/page.tsx
@@ -1,14 +1,8 @@
 'use client'
 import TableConsult from '@/components/tablesComponents/tableConsult'
+import { notFound } from 'next/navigation'
 
 const AnthropometryPage = () => {
-  const patient = {
-    name: 'Guilherme Xavier Martins',
-    age: 25,
-    gender: 'Masculino',
-    cpf: '110.525.576-00',
-  }
-
   return <TableConsult rowKey={'id'} data={[]} columns={[]} />
 }
 
--- src/app/(dashboard)/patients/[id]/layout.tsx
+++ src/app/(dashboard)/patients/[id]/layout.tsx
@@ -1,27 +1,30 @@
-'use client'
+import React, { ReactElement } from 'react'
 import TopDash from '@/components/layoutComponents/topDash'
 import MenuConsult from '@/components/consult-components/menu'
-import TableConsult from '@/components/tablesComponents/tableConsult'
 import Person from '@mui/icons-material/Person'
 
-export default function ConsultLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+type User = {
+  name: string
+  age: number
+  gender: string
+}
+
+type ConsultLayoutProps = {
+  children: ReactElement
+}
+
+export default async function ConsultLayout({ children }: ConsultLayoutProps) {
   return (
     <div className="w-full h-full flex flex-col">
       <TopDash
-        title="Guilherme Xavier Martins"
-        description="Masculino, 22 anos"
+        title={'Guilherme Xavier Martins'}
+        description={'160 an0s'}
         icon={Person}
-        // onClick={() => {}}
-        // textBtn='Voltar'
-        // btnIcon={ArrowBackIcon}
       />
 
       <div className="h-full w-full flex gap-4">
         {children}
+
         <div className="h-full flex justify-end">
           <MenuConsult />
         </div>
