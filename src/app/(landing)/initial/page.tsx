8b052714b5af3fc1fff460dc2fbdc23f92268e8f
Guilherme Xavier <xguilherme1@gmail.com>
2025-08-11 12:22:44 -0300

    metadata

--- src/app/(landing)/initial/page.tsx
+++ src/app/(landing)/initial/page.tsx
@@ -1,4 +1,37 @@
-import React from 'react'
+export const metadata = {
+  metadataBase: new URL('https://uppinghealth.com'),
+  title: 'UppingHealth - Gestão em Saúde',
+  description:
+    'UppingHealth é o sistema completo de gestão em saúde que otimiza agendamentos, prontuários eletrônicos e processos administrativos com segurança e eficiência.',
+  openGraph: {
+    title: 'UppingHealth - Sistema de Gestão em Saúde para Clínicas',
+    description:
+      'Otimize sua clínica ou hospital com UppingHealth. Gestão completa com agendamentos, prontuários eletrônicos, faturamento e mais.',
+    url: 'https://uppinghealth.com',
+    siteName: 'UppingHealth',
+    images: [
+      {
+        url: 'https://uppinghealth.com/imagem-preview.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'pt-BR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title:
+      'UppingHealth - Sistema de Gestão em Saúde para Clínicas e Hospitais',
+    description:
+      'Otimize sua clínica ou hospital com UppingHealth. Gestão completa com agendamentos, prontuários eletrônicos, faturamento e mais.',
+    images: ['https://uppinghealth.com/imagem-preview.png'],
+  },
+  icons: {
+    icon: '/favicon.ico',
+  },
+}
+
 import HeaderLanding from './_components/Header'
 import HeroLanding from './_components/Hero'
 import Benefits from './_components/Benefits'
@@ -6,7 +39,7 @@ import Plans from './_components/Plans'
 import Avaliations from './_components/Avaliations'
 import Footer from './_components/Footer'
 
-const Initial = () => {
+export default function Initial() {
   return (
     <main className="font-sans">
       <HeaderLanding />
@@ -18,5 +51,3 @@ const Initial = () => {
     </main>
   )
 }
-
-export default Initial
