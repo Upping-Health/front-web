9ec11bfd06f5a2df6672d8b6a9fab1d12ecb5df1
Guilherme Xavier <xguilherme1@gmail.com>
2025-08-11 12:15:49 -0300

    ajuste redirect initial

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,11 +6,10 @@ export default async function middleware(request: NextRequest) {
   const signInURL = new URL('/login', request.url)
   const dashboardURL = new URL('/dashboard', request.url)
 
-  const publicPaths = ['/login', '/register', '/accountRecovery', '/initial']
+  const publicPaths = ['/login', '/register', '/accountRecovery']
   const currentPath = request.nextUrl.pathname
 
-  console.log(currentPath)
-  console.log(publicPaths.includes(currentPath))
+  if (currentPath === '/initial') return NextResponse.next()
 
   if (currentPath === '/') {
     return NextResponse.redirect(signInURL)
